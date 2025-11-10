6b019ddf6fc09897522efdeae47bbc3df64692c8
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-10 09:17:46 +0530

    basic structure

---
+++ export import and export using js files/app.js
@@ -0,0 +1,8 @@
+const name = 'radhika';
+
+// named export
+export const phone_no = 8708023986;
+export const dob = '20 dec 2005';
+
+// default named export .
+export default name;
---
+++ export import and export using js files/script.js
@@ -0,0 +1,11 @@
+// default import
+import username from './app.js';
+
+//named import
+import {phone_no , dob} from './app.js';
+// import {dob} from './app.js';
+
+// console.log('hello');
+console.log(phone_no,username,dob);
+// console.log(username);
+console.log(dob);
