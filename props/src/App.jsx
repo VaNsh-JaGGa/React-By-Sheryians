5636c1f756fbafe7bb4622fb949d6b46b1f32e74
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-10 12:45:53 +0530

    props - 1 commit (card made)

---
+++ props/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/card"
+
+const App = () => {
+  return (
+    <div className="parent">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ props/src/components/Card.jsx
@@ -0,0 +1,16 @@
+
+const Card = () => {
+  return (
+    <div>
+         <div className="Child1">
+        <img src="https://plus.unsplash.com/premium_photo-1728892768695-ebebed48ff90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735" alt="" />
+        <h1>react</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, hic? Voluptate, consectetur.</p>
+        <button> Handle </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Card;
