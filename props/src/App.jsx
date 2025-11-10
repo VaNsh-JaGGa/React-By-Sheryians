688c99c6c1403c7cc5e921a92809e598f078677d
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-10 18:40:32 +0530

    props concept

--- props/src/App.jsx
+++ props/src/App.jsx
@@ -3,11 +3,11 @@ import Card from "./components/card"
 const App = () => {
   return (
     <div className="parent">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card user = 'vansh' img = 'https://images.unsplash.com/photo-1759339433160-7a5828396250?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170'/>
+      <Card user = 'jagga' img = 'https://images.unsplash.com/photo-1741732311355-808c468deb4e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687' />
+      <Card user = 'winner' img = 'https://plus.unsplash.com/premium_photo-1762456150958-d0aefc32ce03?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687' />
+      <Card user = 'jattni' img = 'https://images.unsplash.com/photo-1761405378790-684eceaad81c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764' />
+      <Card user = 'badmash' img = 'https://images.unsplash.com/photo-1762273309921-65bb52f1942c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687' />
     </div>
   )
 }
--- props/src/components/Card.jsx
+++ props/src/components/Card.jsx
@@ -1,10 +1,13 @@
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props);
+  console.log(props.user);
+
   return (
     <div>
          <div className="Child1">
-        <img src="https://plus.unsplash.com/premium_photo-1728892768695-ebebed48ff90?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=735" alt="" />
-        <h1>react</h1>
+        <img src = {props.img} alt = "" />
+        <h1>{props.user}</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, hic? Voluptate, consectetur.</p>
         <button> Handle </button>
       </div>
