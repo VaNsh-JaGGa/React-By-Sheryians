b4c64e265a5e1f947e32001814c45bffabbedd90
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-10 11:19:02 +0530

    1 component added

--- components/src/App.jsx
+++ components/src/App.jsx
@@ -1,24 +1,11 @@
+import Card from './components/card';
 
 const App = () => {
   return (
     <div>
-      <div className="card1">
-        <h1>VIRAT KOHLI</h1>
-        <p>unbeatable</p>
-      </div>
-
-      <div className="card1">
-        <h1>VIRAT KOHLI</h1>
-        <p>unbeatable</p>
-      </div>
-      
-      <div className="card1">
-        <h1>VIRAT KOHLI</h1>
-        <p>unbeatable</p>
-      </div>
-
+      <Card />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ components/src/components/Card.jsx
@@ -0,0 +1,13 @@
+
+const Card = () => {
+  return (
+      <div>
+      <div className="card1">
+        <h1>VIRAT KOHLI</h1>
+        <p>unbeatable</p>
+      </div>
+      </div>
+  )
+}
+
+export default Card;
