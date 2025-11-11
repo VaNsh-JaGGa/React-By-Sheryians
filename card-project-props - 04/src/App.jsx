09ce63a0cac8a787fdc7a6ba93bcac192698c878
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-11 13:42:19 +0530

    css added in card

--- card-project-props - 04/src/App.jsx
+++ card-project-props - 04/src/App.jsx
@@ -1,35 +1,16 @@
-import { Bookmark } from 'lucide-react'
+import Card from './components/Card'
 
 const App = () => {
   return (
     <div className="parent">
-
-      <div className="card">
-
-        <div className="top">
-          <img src="https://imgs.search.brave.com/seiMig0aND-rc8Dy7OGiRJjZPFp8swDPN5NevDXm6aA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMTkv/MTM2LzMyMi9zbWFs/bC9hbWF6b24tbG9n/by1hbWF6b24taWNv/bi1mcmVlLWZyZWUt/dmVjdG9yLmpwZw" alt="" />
-          <button>Save <Bookmark /></button>
-        </div>
-
-        <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
-          </div>
-        </div>
-
-        <div className="bottom">
-          <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
-          </div>
-          <button>Apply Now</button>
-        </div>
-
-      </div>
-
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
