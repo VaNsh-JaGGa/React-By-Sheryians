0af84292675eaf968eccd782174538b4beecbf00
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-16 22:03:57 +0530

    functions ended

--- functions - 08/src/App.jsx
+++ functions - 08/src/App.jsx
@@ -1,9 +1,26 @@
 
 const App = () => {
+  const btnClicked = () =>{
+    console.log("button clicked");
+    
+  }
+
+  const mouseEntered = () =>{
+    console.log("mouse entered");
+  }
+
+  const showingChanges = (val) => {
+    // console.log('changing');
+    console.log(val);
+  }
   return (
     <div>
-      App
+      <button onMouseEnter={mouseEntered} onClick={btnClicked} >Click here</button>
+      <input type="text" placeholder="enter the name" onChange={(elem)=>{console.log(elem);console.log(elem.target);console.log(elem.target.value);
+      }} />
+      <input type="text" placeholder="hellll" onChange={(elem)=>{showingChanges(elem.target.value)}} />
     </div>
+
   )
 }
 
