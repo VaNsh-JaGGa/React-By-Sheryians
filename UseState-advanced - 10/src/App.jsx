e8806ed11dda458a2268dfa5aa13da31f1b592af
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-17 22:27:23 +0530

    counter project

--- UseState-advanced - 10/src/App.jsx
+++ UseState-advanced - 10/src/App.jsx
@@ -3,30 +3,31 @@ import { useState } from "react"
 
 const App = () => {
 
-  //conclude of 1 ---- bhaiya agar website me changes ko show karna hai use state ka use krlon na
-  // -----1
-  // let a=20
+const [num, setnum] = useState(0)
 
-  // const changeA = ()=>{
-  //   console.log(a);
-  //   a=30;
-  //   console.log(a);
-  // }
+const increaseNum = () => {
+  setnum(num + 1);
+}
 
-  // step 1 -- usestate enter karo react vala libraries import karane k lie
-  // step 2 -- usess type karo usestatesnippet ko enetrr karo or use state ka syntax aajega
-  // step 3 -- first value of array is read , second one is write jisme ap react ko batate hai ki bhai ye change kardon phir voh pre build functions ki help se voh changes kar deta hai
-  usess
-  const [a, setA] = useState(20)
-  const changeA = () =>{
-    setA(40);
-  }
+const decreaseNum = () => {
+  setnum(num - 1);
+}
+
+const jumpBy5 = () =>{
+  setnum(num+5);
+}
   return (
     <div>
-      {/* ----1 */}
-      {/* <h1>{a}</h1>
-      <button onClick={()=>{changeA()}}>click Me</button> */}
-      <h1 onClick={()=>{changeA()}}>the value of a is {a}</h1>
+      <h1>{num}</h1>
+      <button onClick={()=>{
+          increaseNum();
+      }}>Increase</button>
+      <button onClick={()=>{
+        decreaseNum();
+      }}>Decrease</button><br />
+      <button className="one" onClick={()=>{
+        jumpBy5();
+      }}>Jump by 5</button>
     </div>
   )
 }
