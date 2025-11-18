904993c98a0531b11312d45f2ae832799282598b
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-11-19 00:10:49 +0530

    final

--- max level of use state - 11/src/App.jsx
+++ max level of use state - 11/src/App.jsx
@@ -2,26 +2,40 @@ import { useState } from "react";
 
 const App = () => {
 
-  const [num, setnum] = useState(10)
+  // const [num, setnum] = useState(10)
+  const [num, setnum] = useState({user:"vansh",age:20})
 
   const btnClicked = ()=>{
-    console.log("button clicked");
+
+
+    const newNum = {...num};
+    console.log(newNum);
+    newNum.user="jagga";
+    newNum.age=24;
+    setnum(newNum);
+    setnum(prev=>({...prev,age:30}));
+    // console.log("button clicked");
     //prints 10 on console
-    console.log(num);
-    setnum(20);
+    // console.log(num);
     // * setnum(num) -- same value dubara dalunga too re render ni krega react
     // * ye 1 step piche chalta hai
     // prints 10 on console still beacause the setnum is a ashynchronous in nature
-    console.log(num);
-    
+    // console.log(num);
   }
 
   return (
     <div>
-    <h1>{num}</h1>
+
+    <h1 onClick={()=>{
+      btnClicked();
+    }}>{num.user}</h1>
+    <h1>{num.age}</h1>
+    
+    {/* <h1>{num}</h1>
     <button onClick={()=>{
       btnClicked();
-    }}>click me</button>
+    }}>click me</button> */}
+
     </div>
   )
 }
