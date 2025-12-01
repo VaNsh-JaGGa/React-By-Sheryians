b79576d7c1189618793cd66ca8b8d8728d35d3d9
VaNsh-JaGGa <vanshjagga66@gmail.com>
2025-12-02 00:18:46 +0530

    6:41:00

--- notes-app-14/src/App.jsx
+++ notes-app-14/src/App.jsx
@@ -7,10 +7,11 @@ const App = () => {
   }
 
   return (
-    <div className="h-screen bg-black text-white">
+    <div className="h-screen lg:flex bg-black text-white">
       <form onSubmit={(e)=>{
         SubmitHandler(e);
-      }} className="flex items-start gap-4 flex-col justify-between p-10">
+      }} className="flex items-start lg:w-1/2 gap-4 flex-col p-10">
+         <h1 className="text-3xl font-bold">Add Notes</h1>
           <input
             type="text"
             placeholder="Enter Notes Heading"
@@ -26,8 +27,14 @@ const App = () => {
           <button className="bg-white w-full text-black px-5 py-2 rounded">Add Notes</button>
         {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH2jD4jHXqii8nhc6QYUx7CM23J4mLa4ssYw&s" alt="" /> */}
       </form>
-      <div>
-        <div className=""></div>
+      <div className="lg:w-1/2 lg:border-l-2 p-10">
+      <h1 className="text-3xl font-bold">Recent Notes</h1>
+      <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
+        <div className="h-50 w-42 rounded-2xl bg-white"></div>
+        <div className="h-50 w-42 rounded-2xl bg-white"></div>
+        <div className="h-50 w-42 rounded-2xl bg-white"></div>
+        <div className="h-50 w-42 rounded-2xl bg-white"></div>
+      </div>
       </div>
     </div>
   )
